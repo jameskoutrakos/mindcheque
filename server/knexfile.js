fae2bd5b003ce0ff5cf7ae2aa68d471bac2b437c
jameskoutrakos <jameskoutrakos@gmail.com>
2021-11-23 23:45:39 -0500

    Created .js files for the server-side of my web app. Will need to inquire on mySQL next steps with teaching team.

---
+++ server/knexfile.js
@@ -0,0 +1,14 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "mysql",
+    connection: {
+      host: "127.0.0.1",
+      database: "mindcheque",
+      user: "root",
+      password: "rootroot",
+      charset: "utf8",
+    },
+  },
+};
